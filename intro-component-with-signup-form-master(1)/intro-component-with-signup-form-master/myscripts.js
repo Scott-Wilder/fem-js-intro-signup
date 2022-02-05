54e7b07c985d8cad7e6e62f07bff4fe57bba1ac6
Scott Wilder <53450610+Scott-Wilder@users.noreply.github.com>
2022-02-05 11:10:27 -0500

    style: Feature and updates related to styling

--- intro-component-with-signup-form-master(1)/intro-component-with-signup-form-master/myscripts.js
+++ intro-component-with-signup-form-master(1)/intro-component-with-signup-form-master/myscripts.js
@@ -1,7 +1,40 @@
-var x = 5;
+const form = document.getElementById('form');
+const fname = document.getElementById('fname');
+const lname = document.getElementById('lname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
 
-test(x);
+if(form) {
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  checkInputs();
+});
+}
 
-function test(x) {
-  return console.log(x);
+function checkInputs() {
+  // get values from the inputs
+  const fnameValue = fname.value.trim();
+  const lnameValue = lname.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (fnameValue === '') {
+    // show error
+    // add error class
+    setErrorFor(fname, 'First Name cannot be blank');
+  } else {
+    // add success class
+    setSuccessFor(fname);
+  }
+}
+// error class
+function setErrorFor(input, message) {
+  const formControl = input.parentElement; // .form-control
+  const small = formControl.querySelector('small');
+
+  //add error message inside small
+  small.innerText = message;
+
+  //add error class
+  formControl.className = 'form-control error';
 }
