81496513b9d74bcdd05608cec90b56ad94e32e87
Scott Wilder <53450610+Scott-Wilder@users.noreply.github.com>
2022-02-06 10:07:52 -0500

    style: Feature and updates related to styling

--- intro-component-with-signup-form-master(1)/intro-component-with-signup-form-master/README-template.md
+++ intro-component-with-signup-form-master(1)/intro-component-with-signup-form-master/README-template.md
@@ -76,7 +76,7 @@ Use this section to outline areas that you want to continue focusing on in futur
 ### Useful resources
 
 - [Example resource 1](https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation) - This helped me understand the difference between client and server side form validation.
-- [Example resource 2](https://www.example.com) - This is an amazing article which helped me finally understand XYZ. I'd recommend it to anyone still learning this concept.
+- [Example resource 2](https://www.youtube.com/watch?v=rsd4FNGTRBw&t=1322s) - This is a good video explaining JavaScript Client-side Form Validation.
 
 **Note: Delete this note and replace the list above with resources that helped you during the challenge. These could come in handy for anyone viewing your solution or for yourself when you look back on this project in the future.**
 
--- intro-component-with-signup-form-master(1)/intro-component-with-signup-form-master/myscripts.js
+++ intro-component-with-signup-form-master(1)/intro-component-with-signup-form-master/myscripts.js
@@ -26,6 +26,28 @@ function checkInputs() {
     // add success class
     setSuccessFor(fname);
   }
+  if (lnameValue === '') {
+    // show error
+    // add error class
+    setErrorFor(lname, 'Last Name cannot be blank');
+  } else {
+    // add success class
+    setSuccessFor(lname);
+  }
+  if (emailValue === '') {
+    setErrorFor(email, 'Email cannot be blank');
+  } else if(!isEmail(emailValue)) {
+    setErrorFor(email, 'Email is not valid');
+  } else {
+    setSuccessFor(email);
+  }
+  if (passwordValue === ''){
+    setErrorFor(password, 'Password cannot be blank');
+  } else if (passwordValue.length < 5) {
+    setErrorFor(password, 'Password cannot be less than 5 characters');
+  } else {
+    setSuccessFor(password);
+  }
 }
 // error class
 function setErrorFor(input, message) {
@@ -37,4 +59,13 @@ function setErrorFor(input, message) {
 
   //add error class
   formControl.className = 'form-control error';
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement
+  formControl.className = 'form-control success';
+}
+
+function isEmail(email) {
+  return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
 }
